da507d4f530dd1d4fbd087a74f80fcb53d192cbf
Leo Lugo <leo.lugo84@gmail.com>
2018-04-07 16:29:18 -0700

    semantin done

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,23 +1,121 @@
 import React, { Component } from 'react'
-import List from '../List'
+import {getList} from '../../actions/GetList/GetList'
+import {connect} from 'react-redux'
+import axios from 'axios'
+import {List, Button, Form, Checkbox} from 'semantic-ui-react'
+import './styles.css'
+import {Link} from 'react-router-dom'
+
 
 class App extends Component {
 
-  addTask = (e) => {
-    e.preventDefault()
-    console.log(e.target.value)
+  state = {
+    itemtext: "",
+    completed: false
+  }
+  componentDidMount(){
+    getList()
   }
 
+  addTask = (e) => {
+    e.preventDefault()
+
+    if(this.state.itemtext !== ""){
+      axios.post('http://localhost:3001/listitems', {
+        itemtext : this.state.itemtext,
+        completed: this.state.completed
+      }).then(resp => {
+        getList()
+      }).catch(e => {
+        console.log(e)
+      })
+    }
+
+    this.setState({
+      itemtext: ""
+    })
+  }
+ 
+  manageChange = (e) => {
+    this.setState({
+      itemtext: e.target.value
+    })
+  }
+  
+
+
+
   render() {
+
+
     return (
-        <div>
-          <form>
-            <input type="text" onBlur={this.addTask}/>
-            </form>
-          <List />
+        <div className="main">
+          <div className="content">
+          <h1>To Do List</h1>
+            <Form onSubmit={this.addTask} id="onlyform"  className="holdtext">
+              <input type="text" value={this.state.itemtext} name="itemtext" onChange={this.manageChange}/><Form.Button content='Add To List' className="butt"/>
+            </Form>
+            <List animated celled size='big'>
+            {this.props.listitems.map(item => (
+              <Item {...item} key={'item' + item.id} />
+              ))}
+            </List>
+            <div>
+              <Button.Group size='large'>
+                <Button>one</Button>
+                <Button>two</Button>
+                <Button>three</Button>
+              </Button.Group>
+            </div>
+          </div>
         </div>
     )
   }
 }
 
-export default App
+function mapStateToProps(state) {
+  return{
+   listitems: state.listitems
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
+
+
+
+class Item extends Component {
+
+  manageClick = (e) => {
+      axios.patch(`http://localhost:3001/listitems/${this.props.id}`, {
+        completed: !this.props.completed
+      }).then(resp => {
+        getList()
+      })
+    }
+     deleteTask = (e) => {
+    axios.delete(`http://localhost:3001/listitems/${this.props.id}`).then(resp => {
+      getList()
+    })
+  }
+
+
+
+  render() {
+    return(
+          <List.Item>
+              <Checkbox toggle onChange={this.manageClick} label={this.props.itemtext} checked={this.props.completed} name="completed" value={this.props.id}/><Button floated='right' basic color='blue' type="button" value={this.props.id} onClick={this.deleteTask}>Delete</Button>
+              </List.Item>
+
+      )
+  }
+
+}
+
+
+
+
+
+
+
+
